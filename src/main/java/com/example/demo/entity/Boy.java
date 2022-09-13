56d7bccb0cb00e1072f7d5261687dd99bbc444eb
guoyg <@>
2022-09-13 11:45:48 +0800

    序列化和反序列化

--- src/main/java/com/example/demo/entity/Boy.java
+++ src/main/java/com/example/demo/entity/Boy.java
@@ -3,6 +3,8 @@ package com.example.demo.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @program: springboot_01
  * @description:
@@ -10,14 +12,29 @@ import lombok.Data;
  * @create: 2021-01-14 14:43
  **/
 @Data
-public class Boy {
+public class Boy   implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
     private String name;
     private String sex2;
     private String createdTimeStr;
     private String chaodai;
     private Integer sex;
+    // 新增加 的 属性
+    private String newAttr;
 
-
-
-
+    // 新增另一个 的 属性
+    private String newAttr2;
+    @Override
+    public String toString() {
+        return "Boy{" +
+                "name='" + name + '\'' +
+                ", sex2='" + sex2 + '\'' +
+                ", createdTimeStr='" + createdTimeStr + '\'' +
+                ", chaodai='" + chaodai + '\'' +
+                ", sex=" + sex +
+                '}';
+    }
 }
